d7f636b21f040dc7ef89333f333d4ca0b84687a0
Liorenday <li.orenday.o@gmail.com>
2025-03-22 18:52:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const images = [
     'https://github.com/Liorenday/liore-crear-pose.github.com/blob/main/01%20-%2052b7cfe2d80a2531268e6fd543be9059.jpg?raw=true',
     'https://github.com/Liorenday/liore-crear-pose.github.com/blob/main/01%20-%2052b7cfe2d80a2531268e6fd543be9059.jpg?raw=true',
+     'https://github.com/Liorenday/liore-crear-pose.github.com/blob/main/01%20-%2052b7cfe2d80a2531268e6fd543be9059.jpg?raw=true',
+    
     
 ];
 
